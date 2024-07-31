bd5c1d44da760586ecf7e780f0b8673054bd3981
Josmin-theresa <josmin@ethan-ai.com>
2024-07-31 17:18:11 +0530

    model evaluation added

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from textsummariser.pipeline.stage_01_data_ingestion import DataIngestionTrainigPipeline
 from textsummariser.pipeline.stage_02_data_validation import DataValidationTrainigPipeline
 from textsummariser.pipeline.stage_03_data_transformation import DataTransformationTrainigPipeline
+from textsummariser.pipeline.stage_04_model_trainer import ModelTrainerTrainingPipeline
+from textsummariser.pipeline.stage_05_model_evaluation import ModelEvaluationTrainingPipeline
 from textsummariser.logging import logger
 
 
@@ -35,3 +37,34 @@ try:
 except Exception as e:
         logger.exception(e)
         raise e
+
+
+
+STAGE_NAME="Model Training stage"
+
+try:
+   logger.info(f"*****************************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   Model_trainer = ModelTrainerTrainingPipeline()
+   Model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+STAGE_NAME="Model Evaluation stage"
+
+try:
+   logger.info(f"*****************************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   Model_trainer = ModelEvaluationTrainingPipeline()
+   Model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
